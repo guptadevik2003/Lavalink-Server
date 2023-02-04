e99ff1740820c32a10d31d2a5e560eb0c24b298d
Devik Gupta <64042688+guptadevik2003@users.noreply.github.com>
2023-02-05 03:51:26 +0530

    Updated to nix

--- start.sh
+++ start.sh
@@ -1,9 +1,3 @@
-## Installing jabba and sht
-echo Downloading Java 16...
-curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash -s -- --skip-rc && . ~/.jabba/jabba.sh
-## Installing Java 16
-jabba install zulu@1.16.0
-echo Java Installed.
 java -version
 npm install
 node index.js
