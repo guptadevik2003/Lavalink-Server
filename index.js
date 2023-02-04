c451ef1411a8b95bb199409ce0097e2f09fe9a79
Devik Gupta <64042688+guptadevik2003@users.noreply.github.com>
2023-02-05 04:35:25 +0530

    Updated to release

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-// https://github.com/freyacodes/Lavalink
+// https://github.com/DarrenOfficial/lavalink-replit
 const fs = require('fs')
 const fetch = require('node-fetch')
-const downloader = require('./downloader')
 const { execSync } = require('child_process')
 require('dotenv').config()
 
 let application = fs.readFileSync('./applicationExample.yml', 'utf-8')
-
 if (!process.env.PORT || !process.env.PASS) return console.log("Update Environment Values")
-
 application = application.replace('ENVPORT', process.env.PORT)
 application = application.replace('ENVPASS', process.env.PASS)
-
 fs.writeFileSync('./application.yml', application, 'utf-8')
 
-downloader()
+console.log('Downloading Lavalink.jar...')
+const file = fs.createWriteStream('./Lavalink.jar');
+fetch('https://cdn.darrennathanael.com/jars/Lavalink.jar').then(res => {
+    res.body.pipe(file)
+    file.on('finish', () => {
+        console.log('Downloaded Lavalink.jar')
+        file.close(start());
+    })
+    file.on('error', (error) => { console.error('File Stream Error while fetching Lavalink.jar: ' + error) })
+}).catch(error => { console.error('Error while fetching Lavalink.jar: ' + error) })
 
-if(!fs.existsSync('./Lavalink.jar')) return console.log('Error downloading Lavalink.jar')
-
-// execSync("java -jar Lavalink.jar", { stdio: "inherit" })
+async function start(){
+    execSync("java -jar Lavalink.jar", { stdio: "inherit" })
+}
